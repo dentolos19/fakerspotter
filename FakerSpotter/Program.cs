d62ab210236fece865cc5324703f0e1d28cf2e3f
Dennise Catolos <63337204+dentolos19@users.noreply.github.com>
2021-07-28 19:17:32 +0800

    Removed blazorise & Added bootstrap and fontawesome

--- FakerSpotter/Program.cs
+++ FakerSpotter/Program.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Blazorise;
-using Blazorise.Bootstrap;
-using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -16,9 +13,8 @@ namespace FakerSpotter
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.Services.AddBlazorise(options => { options.ChangeTextOnKeyPress = true; }).AddBootstrapProviders().AddFontAwesomeIcons();
-            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri( builder.HostEnvironment.BaseAddress ) });
             builder.RootComponents.Add<App>("#app");
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             await builder.Build().RunAsync();
         }
 
